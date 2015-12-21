3c329730797b0bf25835e27c435781f78c21d772
mortenjust <morten.just@genstart.dk>
2015-12-20 18:39:38 -0800

    Initial Commit

---
+++ Photometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photometer
+//
+//  Created by Morten Just Petersen on 12/20/15.
+//  Copyright © 2015 Morten Just Petersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
